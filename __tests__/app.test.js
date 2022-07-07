e898b9564f879aa271893e26da83b9eda8933a87
TrustTheProcess712 <marshall1610@hotmail.co.uk>
2022-07-07 10:34:40 +0100

    addded work in progress

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const request = require("supertest");
 const app = require("../app");
 const db = require("../db/connection");
 const testData = require("../db/data/test-data");
+const reviews = require("../db/data/test-data/reviews");
 const seed = require("../db/seeds/seed");
 
 beforeEach(() => {
@@ -242,6 +243,76 @@ describe("GET /api/reviews/:review_id with a comment_count", () => {
       .expect(400)
       .then(({ body: { msg } }) => {
         expect(msg).toBe("Bad Request, Invalid Input");
+      });
+  });
+});
+
+describe("GET /api/reviews/:review_id", () => {
+  test("status:200, responds with a single review object", () => {
+    const review_id = 2;
+    return request(app)
+      .get(`/api/reviews/${review_id}`)
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.review).toEqual({
+          review_id: review_id,
+          title: "Jenga",
+          designer: "Leslie Scott",
+          owner: "philippaclaire9",
+          review_img_url:
+            "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
+          review_body: "Fiddly fun for all the family",
+          category: "dexterity",
+          created_at: "2021-01-18T10:01:41.251Z",
+          votes: 5,
+          comment_count: 3,
+        });
+      });
+  });
+  test("status:400, responds with a bad request error message when passed a bad review ID", () => {
+    return request(app)
+      .get("/api/reviews/notAniD")
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Bad Request, Invalid Input");
+      });
+  });
+  test("status:404, respond with an error message when passed a valid ID number that is not found", () => {
+    return request(app)
+      .get("/api/reviews/9999")
+      .expect(404)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Review not found for review_id: 9999");
+      });
+  });
+});
+
+describe("GET /api/reviews", () => {
+  test("status:200, responds with an array of objects containing review data in descending order", () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then(({ body: { reviews } }) => {
+        expect(reviews.length).toBeGreaterThan(0);
+        expect(reviews).toBeSortedBy("created_at", {
+          descending: true,
+        });
+        reviews.forEach((review) => {
+          expect(review).toEqual(
+            expect.objectContaining({
+              review_id: expect.any(Number),
+              title: expect.any(String),
+              designer: expect.any(String),
+              owner: expect.any(String),
+              review_img_url: expect.any(String),
+              review_body: expect.any(String),
+              category: expect.any(String),
+              created_at: expect.any(String),
+              votes: expect.any(Number),
+              comment_count: expect.any(Number),
+            })
+          );
+        });
       });
   });
 });
