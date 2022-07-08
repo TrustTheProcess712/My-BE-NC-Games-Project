106fe44d3a6bf207209df7dabad2be5e70ccc218
TrustTheProcess712 <marshall1610@hotmail.co.uk>
2022-07-08 15:57:12 +0100

    added listen update

--- listen.js
+++ listen.js
@@ -1,5 +1,8 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 5432;
+const { PORT = 9090 } = process.env;
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}....`));
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`Listening on ${PORT}...`);
+});
